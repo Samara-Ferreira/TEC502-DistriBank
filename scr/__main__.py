fec25ffc1d5049c2addae267dc7ec34a9853e5a1
Samara <sferreira@ecomp.uefs.br>
2024-06-24 21:50:52 -0300

    :sparkles: feat: adição do arquivo principal do projeto

---
+++ scr/__main__.py
@@ -0,0 +1,31 @@
+'''
+Autora: Samara dos Santos Ferreira
+Última modificação: 2024-06-22
+Descrição: Arquivo principal do projeto, onde é iniciado o servidor da API do banco financeiro.
+'''
+
+# Importar as bibliotecas necessárias
+from time import sleep
+#import Consortium.API.API as API
+from Consortium.API import API
+from os import system, name
+
+
+# Limpar o terminal antes de iniciar
+def clear():
+    # no windows e no linux
+    system('cls' if name == 'nt' else 'clear')
+
+
+# Iniciar o servidor da API
+if __name__ == "__main__":
+    try:
+        clear()
+        cnpj = API.get_cnpj()
+        print(f"API do banco financeiro rodando na porta {API.PORT}, com IP {cnpj}")
+        API.app.run(host="0.0.0.0", port=API.PORT, debug=False, threaded=True)
+
+    except KeyboardInterrupt as e:
+        print("API encerrada!")
+        sleep(0.5)
+        exit(0)
