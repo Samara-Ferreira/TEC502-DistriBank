2059cd01c5a2f2749279930ac83d9ed3ae7e54b7
Samara <sferreira@ecomp.uefs.br>
2024-06-30 15:32:09 -0300

    :sparkles: feat: renomeação do arquivo da classe das filas

--- src/consortium/Queue.py
+++ src/consortium/Queue.py
@@ -1,5 +1,5 @@
 '''
-Descrição: Arquivo referente à fila de prioridade do banco financeiro, onde são definidas as funções principais
+Descrição: este código é referente à fila de prioridade do banco financeiro, onde são definidas as funções principais
 da fila, como inserir um elemento na fila, ordenar a fila, e comparar elementos da fila.
 '''
 
@@ -11,11 +11,13 @@ class QueueBank:
         self.queue = []
         self.queue_exec = []
 
+    # Método para inserir um elemento na fila de forma ordenada
     def insert_order(self, operation):
         pos = self.binary_search(operation)
         self.queue.insert(pos, operation)
         self.lenght += 1
 
+    # Método para fazer uma busca binária na fila para definir a posição
     def binary_search(self, operation):
         left, right = 0, len(self.queue) - 1
         while left <= right:
