4cc2f00fcf9c5be9d117ec05c9ef0579d4ad3a46
Samara <sferreira@ecomp.uefs.br>
2024-06-30 15:31:28 -0300

    :sparkles: feat: adição dos comentários na classe para criar os IDs das operações

--- src/consortium/UniqueTwoDigitID.py
+++ src/consortium/UniqueTwoDigitID.py
@@ -1,13 +1,15 @@
 '''
-Descrição: Arquivo que contém a classe para a criação de contas de pessoas físicas.
-
+Descrição: este código contém a classe para a criação de um identificador único de dois dígitos
+para as operações do banco.
 '''
 
 
+# Classe para a criação de um identificador único de dois dígitos
 class UniqueTwoDigitID:
     def __init__(self):
         self.current_id = 0
 
+    # Método para gerar um identificador único
     def generate(self):
         unique_id = self.current_id
         self.current_id += 1
