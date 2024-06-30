4fc3209e2fcbcf8e1d116d3e44ae66a53a894073
Samara <sferreira@ecomp.uefs.br>
2024-06-30 15:30:46 -0300

    :sparkles: feat: mudanças na classe do relógio vetorial
    - Mudanças nos comentários e no nome da classe.

--- src/consortium/VectorialClock.py
+++ src/consortium/VectorialClock.py
@@ -1,37 +1,37 @@
 '''
-    Descrição: Código para ordenação total de eventos em um sistema distribuídos, utilizando relógio vetorial.
+Descrição: este código foi criado para a ordenação total de eventos em um sistema distribuídos, utilizando relógio
+vetorial.
 '''
 
-
-# Importar as bibliotecas necessárias
+# Importar a biblioteca necessária
 from threading import Lock
 
 
 # Classe para relógio vetorial
-class VectorClock:
+class VectorialClock:
     def __init__(self, num_processes, process_id):
         self.clock = [0] * num_processes
         self.process_id = process_id
         self.num_processes = num_processes
         self.lock = Lock()
 
-    # método para incrementar o relógio
+    # Método para incrementar o relógio
     def increment(self):
         with self.lock:
             self.clock[self.process_id] += 1
             return self.clock.copy()
 
-    # método para atualizar o relógio
+    # Método para atualizar o relógio
     def update(self, other_clock):
         with self.lock:
             for i in range(len(self.clock)):
                 self.clock[i] = max(self.clock[i], other_clock[i])
                 return self.clock.copy()
 
-    # método para obter o relógio
+    # Método para obter o tempo atual do relógio
     def get_time(self):
         return self.clock.copy()
 
-    # método para definir o relógio
+    # étodo para definir o relógio
     def __repr__(self):
         return str(self.clock)
