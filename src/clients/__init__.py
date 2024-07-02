ade0d8bc49925de991c7858f1b2aa30ab255bf8e
Samara <sferreira@ecomp.uefs.br>
2024-07-01 23:15:11 -0300

    :fire: delete: remoção dos arquivos do caminho antigo

--- src/clients/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Descrição: Arquivo referente a importação de módulos do projeto.
-'''
-
-import os
-import sys
-
-parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(parent_dir)
--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(parent_dir)
