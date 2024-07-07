21a332e405041acc7761fb801686893525391f77
Samara <sferreira@ecomp.uefs.br>
2024-07-07 15:12:04 -0300

    :sparkles: feat: adição de novos testes

--- tests/Accounts.py
+++ tests/Accounts.py
@@ -3,8 +3,11 @@ Arquivo que contém os testes para criação de contas físicas particulares, co
 de outros complementares.
 '''
 
+import json
 import asyncio
 import aiohttp
+import requests
+
 from os import system, name
 
 
@@ -15,6 +18,8 @@ host2 = "172.16.103.2"
 host3 = "172.16.103.4"
 host4 = "172.16.103.5"
 
+host_local = "172.22.208.1"
+
 port1 = 5551
 port2 = 5552
 port3 = 5553
@@ -24,20 +29,131 @@ port4 = 5554
 async def create_request(url):
     async with aiohttp.ClientSession() as session:
         async with session.post(url) as response:
-            return await response.text()
+            return await response.json()
 
 
-async def main():
-    urls = [
-        f"http://{host1}:{port1}/maria/08300000000/maria11/maria123/100.0/create_physical_particular",
-        f"http://{host2}:{port2}/jose/08500000000/jose11/jose123/200.0/create_physical_joint",
-        f"http://{host3}:{port3}/empresa/01400000000000/paula/08400000000/paula11/paula123/300.0/create_juridic_account",
-        f"http://{host4}:{port4}/01400000000000/08700000000/paulo/paulo11/paulo123/create_juridic_employeet",
-    ]
+async def main(num):
+    if num == 1:
+        urls = [
+            # f"http://{host1}:{port1}/maria/08500000000/maria11/maria123/100.0/create_physical_particular",
+            # f"http://{host2}:{port2}/jose/08600000000/jose11/jose123/200.0/create_physical_joint",
+            # f"http://{host2}:{port2}/08600000000/maria/08500000000/maria11/maria123/create_joint_complementary",
+            # f"http://{host3}:{port3}/empresa/01400000000000/paula/paula11/08700000000/paula123/300.0/create_juridic_account",
+            # f"http://{host4}:{port3}/01400000000000/paulo/paulo11/08800000000/paulo123/create_juridic_employee",
+            # f"http://{host4}:{port4}/lucas/08900000000/lucas11/lucas123/500.0/create_physical_particular"
+
+            f"http://{host_local}:{port1}/maria/08500000000/maria11/maria123/100.0/create_physical_particular",
+            f"http://{host_local}:{port2}/jose/08600000000/jose11/jose123/200.0/create_physical_joint",
+            f"http://{host_local}:{port2}/08600000000/maria/08500000000/maria11/maria123/create_joint_complementary",
+            f"http://{host_local}:{port3}/empresa/01400000000000/paula/paula11/08700000000/paula123/300.0/create_juridic_account",
+            f"http://{host_local}:{port3}/01400000000000/paulo/paulo11/08800000000/paulo123/create_juridic_employee",
+            f"http://{host_local}:{port4}/lucas/08900000000/lucas11/lucas123/500.0/create_physical_particular"
+        ]
+
+    elif num == 2:
+        urls = [
+            # f"http://{host1}:{port1}/08500000000/physical/cpf_cnpj/08500000000/create_pix_key",
+            # f"http://{host2}:{port2}/08600000000/physical_joint/random/null/create_pix_key",
+            # f"http://{host2}:{port2}/08500000000/physical_joint/email/maria@/create_pix_key",
+            # f"http://{host3}:{port3}/08700000000/juridic/phone/75999999999/create_pix_key",
+            # f"http://{host3}:{port3}/08800000000/juridic/email/paulo@/create_pix_key",
+            # f"http://{host4}:{port4}/08900000000/physical/email/lucas@/create_pix_key"
+            
+            f"http://{host_local}:{port1}/08500000000/physical/cpf_cnpj/08500000000/create_pix_key",
+            f"http://{host_local}:{port2}/08600000000/physical_joint/random/null/create_pix_key",
+            f"http://{host_local}:{port2}/08500000000/physical_joint/email/maria@/create_pix_key",
+            f"http://{host_local}:{port3}/08700000000/juridic/phone/75999999999/create_pix_key",
+            f"http://{host_local}:{port3}/08800000000/juridic/email/paulo@/create_pix_key",
+            f"http://{host_local}:{port4}/08900000000/physical/email/lucas@/create_pix_key"
+        ]
+
+    else:
+        urls = []
 
     tasks = [create_request(url) for url in urls]
-    resultados = await asyncio.gather(*tasks)
-    for resultado in resultados:
-        print(resultado)
+    results = await asyncio.gather(*tasks)
+    for result in results:
+        print(json.dumps(result, indent=4, ensure_ascii=False))
 
-asyncio.run(main())
+
+def print_get_clients():
+    # response = requests.get(f"http://{host1}:{port1}/get_clients")
+    # print("Banco 1: ", response.json())
+    # response = requests.get(f"http://{host2}:{port2}/get_clients")
+    # print("Banco 2: ", response.json())
+    # response = requests.get(f"http://{host3}:{port3}/get_clients")
+    # print("Banco 3: ", response.json())
+    # response = requests.get(f"http://{host4}:{port4}/get_clients")
+    # print("Banco 4: ", response.json())
+
+    response = requests.get(f"http://{host_local}:{port1}/get_clients")
+    print("Banco 1: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port2}/get_clients")
+    print("Banco 2: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port3}/get_clients")
+    print("Banco 3: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port4}/get_clients")
+    print("Banco 4: ", print_correctly(response))
+
+
+def print_get_keys(cpf, type):
+    response = requests.get(f"http://{host_local}:{port1}/{cpf}/{type}/get_keys")
+    return print_correctly(response)
+
+
+def print_correctly(response):
+    content = response.json()
+    return json.dumps(content, indent=4, ensure_ascii=False)
+
+
+print("\n\t", "-"*60)
+print("\t\t\tCriação de contas")
+print("\t", "-"*60)
+
+asyncio.run(main(1))
+
+print("\n\t--- Clientes dos bancos ")
+print_get_clients()
+
+
+print("\n\t", "-"*60)
+print("\t\t\tCriação das chaves pix")
+print("\t", "-"*60)
+
+asyncio.run(main(2))
+
+print("\n\t--- Chaves pix dos clientes")
+response = requests.get(f"http://{host_local}:{port1}/08500000000/physical/get_keys")
+response = print_correctly(response)
+print("Cliente 1: ", response)
+
+response = requests.get(f"http://{host_local}:{port2}/08600000000/physical_joint/get_keys")
+response = print_correctly(response)
+print("Cliente 2: ", response)
+
+response = requests.get(f"http://{host_local}:{port2}/08500000000/physical_joint/get_keys")
+response = print_correctly(response)
+print("Cliente 3: ", response)
+
+response = requests.get(f"http://{host_local}:{port3}/08700000000/juridic/get_keys")
+response = print_correctly(response)
+print("Cliente 4: ", response)
+
+response = requests.get(f"http://{host_local}:{port3}/08800000000/juridic/get_keys")
+response = print_correctly(response)
+print("Cliente 5: ", response)
+
+response = requests.get(f"http://{host_local}:{port4}/08900000000/physical/get_keys")
+response = print_correctly(response)
+print("Cliente 6: ", response)
+
+print("\n\t--- Tentar criar a chave pix com chave existente, só que em outro cliente")
+response = requests.post(f"http://{host_local}:{port1}/maria@/physical/email/08500000000/create_pix_key")    # chave B2
+print("Cliente 1: ", print_correctly(response))
+
+print("\tRetorno")
+response = requests.get(f"http://{host_local}:{port1}/08500000000/physical/get_keys")
+response = print_correctly(response)
+print("Cliente 1: ", response)
+
+print("\n\t", "-"*60)
--- tests/Transactions.py
+++ tests/Transactions.py
@@ -1,13 +1,18 @@
+import json
+import asyncio
+import aiohttp
 import requests
-import asyncio # add no docker
-import aiohttp # add no docker
-from time import sleep
 from os import system, name
+
 
 system('cls' if name == 'nt' else 'clear')
 
-host1 = "192.168.0.111"
+host1 = "172.16.103.1"
 host2 = "172.16.103.2"
+host3 = "172.16.103.4"
+host4 = "172.16.103.5"
+
+host_local = "172.22.208.1"
 
 port1 = 5551
 port2 = 5552
@@ -15,30 +20,137 @@ port3 = 5553
 port4 = 5554
 
 
-async def fazer_requisicao(url):
+async def create_request(url):
     async with aiohttp.ClientSession() as session:
         async with session.post(url) as response:
-            return await response.text()
+            return await response.json()
 
 
-async def main(op1, op2):
-    urls = [
-        f"http://{host1}:{port1}/{op1}/create_transfer",
-        f"http://{host1}:{port1}/{op2}/create_transfer",
-    ]
-    tasks = [fazer_requisicao(url) for url in urls]
-    resultados = await asyncio.gather(*tasks)
-    for resultado in resultados:
-        print(resultado)
+async def main(num):
+    if num == 1:
+        urls = [
+            f"http://{host_local}:{port1}/{host_local}/{port1}/08500000000/physical/50.0/create_deposit",
+            f"http://{host_local}:{port2}/{host_local}/{port2}/08600000000/physical_joint/50.0/create_deposit",
+            f"http://{host_local}:{port2}/{host_local}/{port2}/08500000000/physical_joint/50.0/create_deposit",
+            f"http://{host_local}:{port3}/{host_local}/{port3}/08700000000/juridic/50.0/create_deposit",
+            f"http://{host_local}:{port3}/{host_local}/{port3}/08800000000/juridic/50.0/create_deposit",
+            f"http://{host_local}:{port4}/{host_local}/{port4}/08900000000/physical/50.0/create_deposit"
+        ]
 
-operation1 = {"host_recp": host1, "port_recp": port2, "cpf_recp": "08600000000", "type_recp": "physical",
-                         "key_recp": "gu@",
-                         "host_send": host1, "port_send": port1, "cpf_send": "08500000000", "type_send": "physical",
-                         "value": 150.0, "operation": "transfer"}
+    elif num == 2:
+        urls = [
+            f"http://{host_local}:{port1}/{host_local}/{port1}/08500000000/physical/50.0/create_withdraw",
+            f"http://{host_local}:{port2}/{host_local}/{port2}/08600000000/physical_joint/50.0/create_withdraw",
+            f"http://{host_local}:{port2}/{host_local}/{port2}/08500000000/physical_joint/50.0/create_withdraw",
+            f"http://{host_local}:{port3}/{host_local}/{port3}/08700000000/juridic/50.0/create_withdraw",
+            f"http://{host_local}:{port3}/{host_local}/{port3}/08800000000/juridic/50.0/create_withdraw",
+            f"http://{host_local}:{port4}/{host_local}/{port4}/08900000000/physical/50.0/create_withdraw"
+        ]
 
-operation2 = {"host_recp": host1, "port_recp": port3, "cpf_recp": "08700000000", "type_recp": "physical",
-                         "key_recp": "gui@",
-                         "host_send": host1, "port_send": port1, "cpf_send": "08500000000", "type_send": "physical",
-                         "value": 60.0, "operation": "transfer"}
+    elif num == 3:
+        urls = [
+            f"http://{host_local}:{port1}/{pack1}/create_transfer",
+            f"http://{host_local}:{port1}/{pack2}/create_transfer",
+            f"http://{host_local}:{port2}/{pack3}/create_transfer",
+            f"http://{host_local}:{port3}/{pack4}/create_transfer"
+        ]
 
-asyncio.run(main([operation1], [operation2]))
+    else:
+        urls = []
+
+    tasks = [create_request(url) for url in urls]
+    results = await asyncio.gather(*tasks)
+    for result in results:
+        print(json.dumps(result, indent=4, ensure_ascii=False))
+
+
+def print_correctly(response):
+    content = response.json()
+    return json.dumps(content, indent=4, ensure_ascii=False)
+
+
+def print_operations_bank():
+    response = requests.get(f"http://{host_local}:{port1}/get_bank_statement")
+    print("Banco 1: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port2}/get_bank_statement")
+    print("Banco 2: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port3}/get_bank_statement")
+    print("Banco 3: ", print_correctly(response))
+    response = requests.get(f"http://{host_local}:{port4}/get_bank_statement")
+    print("Banco 4: ", print_correctly(response))
+
+
+print("\n\t", "-" * 60)
+print("\t\t\t\tOperações em todas as contas")
+print("\t", "-" * 60)
+
+# Depósito
+# asyncio.run(main(1))
+
+# Saque
+# asyncio.run(main(2))
+
+# Transferência
+
+# conta 1 -> conta 2
+pack1 = [{"host_recp": host_local, "port_recp": port2, "cpf_recp": "08600000000",
+          "type_recp": "physical_joint", "key_recp": "tttec983olabd01auydz4l8n0ezgci45",
+          "host_send": host_local, "port_send": port1, "cpf_send": "08500000000",
+          "type_send": "physical", "value": 10.0, "operation": "transfer"}]
+
+# conta 1 -> conta 3
+pack2 = [{"host_recp": host_local, "port_recp": port2, "cpf_recp": "08500000000",
+          "type_recp": "physical_joint", "key_recp": "maria@",
+          "host_send": host_local, "port_send": port1, "cpf_send": "08500000000",
+          "type_send": "physical", "value": 10.0, "operation": "transfer"}]
+
+# conta 2 -> conta 4
+pack3 = [{"host_recp": host_local, "port_recp": port3, "cpf_recp": "08700000000",
+          "type_recp": "juridic", "key_recp": "75999999999",
+          "host_send": host_local, "port_send": port2, "cpf_send": "08600000000",
+          "type_send": "physical_joint", "value": 10.0, "operation": "transfer"}]
+
+# conta 4 -> conta 5
+pack4 = [{"host_recp": host_local, "port_recp": port4, "cpf_recp": "08900000000",
+          "type_recp": "physical", "key_recp": "lucas@",
+          "host_send": host_local, "port_send": port3, "cpf_send": "08800000000",
+          "type_send": "juridic", "value": 10.0, "operation": "transfer"}]
+
+asyncio.run(main(3))
+
+# saldos finais
+# Conta 1:  70.0
+# Conta 2:  220.0
+# Conta 3:  220.0
+# Conta 4:  300.0
+# Conta 5:  300.0
+# Conta 6:  510.0
+
+print("\n\t--- Operações dos bancos ")
+print_operations_bank()
+
+print("\n\t--- Saldos de cada conta ")
+response = requests.get(f"http://{host_local}:{port1}/08500000000/physical/get_balance")
+print("Conta 1: ", print_correctly(response))
+
+response = requests.get(f"http://{host_local}:{port2}/08600000000/physical_joint/get_balance")
+print("Conta 2: ", print_correctly(response))
+
+response = requests.get(f"http://{host_local}:{port2}/08500000000/physical_joint/get_balance")
+print("Conta 3: ", print_correctly(response))
+
+response = requests.get(f"http://{host_local}:{port3}/08700000000/juridic/get_balance")
+print("Conta 4: ", print_correctly(response))
+
+response = requests.get(f"http://{host_local}:{port3}/08800000000/juridic/get_balance")
+print("Conta 5: ", print_correctly(response))
+
+response = requests.get(f"http://{host_local}:{port4}/08900000000/physical/get_balance")
+print("Conta 6: ", print_correctly(response))
+
+print("\n\t", "-"*60)
+
+
+
+
+
